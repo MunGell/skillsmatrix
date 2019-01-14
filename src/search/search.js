8be9854ca28460e90d0115a569893c9b00150527
Staab <pstaab@deloitte.ie>
2019-01-14 12:12:38 +0000

    importing bootstrap and using checkboxes for search/filter (not functional)

--- src/search/search.js
+++ src/search/search.js
@@ -76,7 +76,13 @@ class Search extends Component {
                                     level
                                 </div>
                                 {this.returnLevels().map((level,index) =>
-                                    <div className="mtx-sidebar__option">{level}</div>
+
+                                    <div className="search-check custom-control custom-checkbox my-1 mr-sm-2">
+                                        <input type="checkbox" value= {level} className="custom-control-input" id={level}/>
+                                        <label className="custom-control-label" for={level}>{level}</label>
+                                    </div>
+
+                                    
                                     )}
                                 
                             </div>
@@ -86,7 +92,10 @@ class Search extends Component {
                                 </div>
 
                                 {this.returnDept().map((dept,index) =>
-                                    <div className="mtx-sidebar__option">{dept}</div>
+                                    <div className="search-check custom-control custom-checkbox my-1 mr-sm-2">
+                                        <input type="checkbox" value= {dept} className="custom-control-input" id={dept}/>
+                                        <label className="custom-control-label" for={dept}>{dept}</label>
+                                    </div>
                                     )}
 
                             </div>
