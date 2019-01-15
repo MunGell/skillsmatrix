0fad96c68dbc08a77e9994fd8b10e64398233c63
endaogorman <eogorman@deloitte.ie>
2019-01-15 17:07:33 +0000

    Prettier, lint + fix existing bugs

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -4,90 +4,109 @@ import UserData from '../data/users.json';
 import './_profile.scss';
 
 class Profile extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            userData: []
-        };
+  constructor(props) {
+    super(props);
 
-        this.onClickHome = this.onClickHome.bind(this);
-    }   
+    this.state = {
+      userData: [],
+    };
 
-    componentWillMount() {
-        this.setState({userData: UserData});
-    }
+    this.onClickHome = this.onClickHome.bind(this);
+  }
 
-    onClickHome() {
-        let { history } = this.props;
-        history.push('/');
-    }
+  componentWillMount() {
+    this.setState({ userData: UserData });
+  }
 
-	render() {
-        var user = this.state.userData[0];
+  onClickHome() {
+    const { history } = this.props;
+    history.push('/');
+  }
 
-		return (
-            <div>
-                <div className="mtx-logo" onClick={this.onClickHome}>nomad<div>.</div></div>  
-                <div className="mtx-half">
-                    <div className="mtx-section">user profile<div>.</div></div>  
-                    <div className="mtx-block mtx-block--top">
-                        <div className="mtx-column">
-                            <img className="mtx-picture" src="./pic.png"></img>
-                        </div>
-                        <div className="mtx-column mtx-column--center">
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">name</div>
-                                <div className="mtx-section-info__content">{user.name}</div>
-                            </div>
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">email</div>
-                                <div className="mtx-section-info__content">{user.email}</div>
-                            </div>
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">level</div>
-                                <div className="mtx-section-info__content">{user.level}</div>
-                            </div>
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">department</div>
-                                <div className="mtx-section-info__content">{user.dept}</div>
-                            </div>
-                        </div>
-                        <div className="mtx-column">
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">current assignment</div>
-                                <div className="mtx-section-info__content">{user.currentAss}</div>
-                            </div>
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">end date</div>
-                                <div className="mtx-section-info__content">{user.endDate}</div>
-                            </div>
-                            <div className="mtx-section-info">
-                                <div className="mtx-section-info__header">assignment history</div>
-                                {user.assHistory.map(i =>
-                                    <div className="mtx-section-info__content">{i.name}</div>
-                                )} 
-                            </div>
-                        </div>
-                    </div>  
-                </div>  
-                <div className="mtx-half--bottom">
-                    <div className="mtx-section">skills<div>.</div></div>  
-				    <div className="mtx-block mtx-block--bottom">
-                        {user.skills.map(i =>
-                            <div className="mtx-skill">
-                                {/*<div className="mtx-section__content">{i.groupDisplayName}</div>*/}
-                                <div className="mtx-skill__rating">
-                                    <div className="mtx-section__header">{i.displayName}</div>
-                                    <Rating initialRating={i.number} readonly={true} emptySymbol={"mtx-rating--empty"} fullSymbol={"mtx-rating--full"} />
-                                </div>
-                            </div>
-                        )} 
-                    </div>  
-                </div>  
-			</div>
-        );
-	}
+  render() {
+    var user = this.state.userData[0];
+
+    return (
+      <div>
+        <div className="mtx-logo" onClick={this.onClickHome}>
+          nomad<div>.</div>
+        </div>
+        <div className="mtx-half">
+          <div className="mtx-section">
+            user profile<div>.</div>
+          </div>
+          <div className="mtx-block mtx-block--top">
+            <div className="mtx-column">
+              <img className="mtx-picture" src="./pic.png" />
+            </div>
+            <div className="mtx-column mtx-column--center">
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">name</div>
+                <div className="mtx-section-info__content">{user.name}</div>
+              </div>
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">email</div>
+                <div className="mtx-section-info__content">{user.email}</div>
+              </div>
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">level</div>
+                <div className="mtx-section-info__content">{user.level}</div>
+              </div>
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">department</div>
+                <div className="mtx-section-info__content">{user.dept}</div>
+              </div>
+            </div>
+            <div className="mtx-column">
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">
+                  current assignment
+                </div>
+                <div className="mtx-section-info__content">
+                  {user.currentAss}
+                </div>
+              </div>
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">end date</div>
+                <div className="mtx-section-info__content">{user.endDate}</div>
+              </div>
+              <div className="mtx-section-info">
+                <div className="mtx-section-info__header">
+                  assignment history
+                </div>
+                {user.assHistory.map(i => (
+                  <div className="mtx-section-info__content" key={i.order}>
+                    {i.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mtx-half--bottom">
+          <div className="mtx-section">
+            skills<div>.</div>
+          </div>
+          <div className="mtx-block mtx-block--bottom">
+            {user.skills.map(i => (
+              <div className="mtx-skill" key={i.order}>
+                {/*<div className="mtx-section__content">{i.groupDisplayName}</div>*/}
+                <div className="mtx-skill__rating">
+                  <div className="mtx-section__header">{i.displayName}</div>
+                  <Rating
+                    initialRating={i.number}
+                    readonly={true}
+                    emptySymbol={'mtx-rating--empty'}
+                    fullSymbol={'mtx-rating--full'}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
--- src/search/search.js
+++ src/search/search.js
@@ -5,147 +5,171 @@ import '../profile/_profile.scss';
 import './_search.scss';
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            userData: []
-        };
+  constructor(props) {
+    super(props);
 
-        this.onClickHome = this.onClickHome.bind(this);
-        this.onClickUser = this.onClickUser.bind(this);
-    }   
+    this.state = {
+      userData: [],
+    };
 
-    componentWillMount() {
-        this.setState({userData: UserData});
-    }
+    this.onClickHome = this.onClickHome.bind(this);
+    this.onClickUser = this.onClickUser.bind(this);
+  }
 
-    onClickHome() {
-        let { history } = this.props;
-        history.push('/');
-    }
+  componentWillMount() {
+    this.setState({ userData: UserData });
+  }
 
-    onClickUser() {
-        let { history } = this.props;
-        history.push('/profile');
-    }
+  onClickHome() {
+    const { history } = this.props;
+    history.push('/');
+  }
 
-    returnLevels(){
-        let levels = [];
+  onClickUser() {
+    const { history } = this.props;
+    history.push('/profile');
+  }
 
-        this.state.userData.forEach((user) => {
-            if(levels.includes(user.level) == false){
-            levels.push(user.level)
-            }});
+  returnLevels() {
+    const levels = [];
 
-        return levels;
-    }
+    this.state.userData.forEach(user => {
+      if (levels.includes(user.level) === false) {
+        levels.push(user.level);
+      }
+    });
 
-    returnDept(){
-        let dept = [];
+    return levels;
+  }
 
-        this.state.userData.forEach((user) => {
-            if(dept.includes(user.dept) == false){
-            dept.push(user.dept)
-            }});
+  returnDept() {
+    const dept = [];
 
-        return dept;
-    }
+    this.state.userData.forEach(user => {
+      if (dept.includes(user.dept) === false) {
+        dept.push(user.dept);
+      }
+    });
 
-    render() {
-        this.returnLevels();
-        return (
-            <div>
-                <div className="mtx-logo" onClick={this.onClickHome}>nomad<div>.</div></div>  
-                <div className="mtx-user" onClick={this.onClickUser}>
-                    <img src="./user.png"></img>
-                </div>
-                <div className="mtx-half">
-                    <div className="mtx-">
-                        <div className="mtx-section">search results<div>.</div></div>  
-                    </div>    
-                    <div className="mtx-main">
-                        <div className="mtx-sidebar">
-                            <div className="mtx-sidebar__top">
-                                <div className="mtx-section__title">
-                                    filter
-                                </div>
-                            </div>
-                            <div className="mtx-sidebar__section">
-                                <div className="mtx-section__title">
-                                    level
-                                </div>
-                                {this.returnLevels().map((level,index) =>
-                                    <div className="search-check custom-control custom-checkbox my-1 mr-sm-2">
-                                        <input type="checkbox" value= {level} className="custom-control-input" id={level}/>
-                                        <label className="custom-control-label" for={level}>{level}</label>
-                                    </div>
-                                    )}                                
-                            </div>
-                            <div className="mtx-sidebar__section">
-                                <div className="mtx-section__title">
-                                    department
-                                </div>
+    return dept;
+  }
 
-                                {this.returnDept().map((dept,index) =>
-                                    <div className="search-check custom-control custom-checkbox my-1 mr-sm-2">
-                                        <input type="checkbox" value= {dept} className="custom-control-input" id={dept}/>
-                                        <label className="custom-control-label" for={dept}>{dept}</label>
-                                    </div>
-                                )}
-
-                            </div>
-                            <div className="mtx-sidebar__section">
-                                <div className="mtx-section__title">
-                                    skill level
-                                </div>
-                                <div className="mtx-rating__filter">
-                                    <Rating initialRating={3} readonly={true} emptySymbol={"mtx-rating--empty"} fullSymbol={"mtx-rating--full"} />
-                                    3+
-                                </div>
-                            </div>
-                            <div className="mtx-sidebar__section">
-                                <div className="mtx-section__title">
-                                    skills
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mtx-results">
-                            <div className="mtx-section__title ">
-                                sort by
-                            </div>  
-                            <div className="mtx-sr__list">
-                                {this.state.userData.map(i =>
-                                    <div className="mtx-sr__container"  onClick={this.onClickUser}>
-                                        <div className="mtx-sr__top">
-                                            <div className="mtx-sr__left">
-                                                <img className="mtx-sr__picture" src="./pic.png"></img>
-                                            </div>
-                                            <div className="mtx-sr__right">
-                                                <div className="mtx-sr__">
-                                                    <div className="mtx-section__title">{i.name}</div>
-                                                    <div className="mtx-sr__">{i.level}</div>
-                                                    <div className="mtx-sr__email">
-                                                        <div>{i.email}</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="mtx-sr__bottom">
-                                            {i.skills.slice(0, 9).map(i =>
-                                                <div className="mtx-sr__skill">{i.name}</div>
-                                            )}  
-                                            ...  
-                                        </div>
-                                    </div>
-                                )} 
-                            </div>  
-                        </div>
-                    </div>    
-                </div>    
+  render() {
+    this.returnLevels();
+    return (
+      <div>
+        <div className="mtx-logo" onClick={this.onClickHome}>
+          nomad<div>.</div>
+        </div>
+        <div className="mtx-user" onClick={this.onClickUser}>
+          <img src="./user.png" />
+        </div>
+        <div className="mtx-half">
+          <div className="mtx-">
+            <div className="mtx-section">
+              search results<div>.</div>
             </div>
-        );
-    }
+          </div>
+          <div className="mtx-main">
+            <div className="mtx-sidebar">
+              <div className="mtx-sidebar__top">
+                <div className="mtx-section__title">filter</div>
+              </div>
+              <div className="mtx-sidebar__section">
+                <div className="mtx-section__title">level</div>
+                {this.returnLevels().map((level, index) => (
+                  <div
+                    key={index}
+                    className="search-check custom-control custom-checkbox my-1 mr-sm-2"
+                  >
+                    <input
+                      type="checkbox"
+                      value={level}
+                      className="custom-control-input"
+                      id={level}
+                    />
+                    <label className="custom-control-label" htmlFor={level}>
+                      {level}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <div className="mtx-sidebar__section">
+                <div className="mtx-section__title">department</div>
+
+                {this.returnDept().map((dept, index) => (
+                  <div
+                    key={index}
+                    className="search-check custom-control custom-checkbox my-1 mr-sm-2"
+                  >
+                    <input
+                      type="checkbox"
+                      value={dept}
+                      className="custom-control-input"
+                      id={dept}
+                    />
+                    <label className="custom-control-label" htmlFor={dept}>
+                      {dept}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <div className="mtx-sidebar__section">
+                <div className="mtx-section__title">skill level</div>
+                <div className="mtx-rating__filter">
+                  <Rating
+                    initialRating={3}
+                    readonly={true}
+                    emptySymbol={'mtx-rating--empty'}
+                    fullSymbol={'mtx-rating--full'}
+                  />
+                  3+
+                </div>
+              </div>
+              <div className="mtx-sidebar__section">
+                <div className="mtx-section__title">skills</div>
+              </div>
+            </div>
+            <div className="mtx-results">
+              <div className="mtx-section__title ">sort by</div>
+              <div className="mtx-sr__list">
+                {this.state.userData.map((i, index) => (
+                  <div
+                    key={index}
+                    className="mtx-sr__container"
+                    onClick={this.onClickUser}
+                  >
+                    <div className="mtx-sr__top">
+                      <div className="mtx-sr__left">
+                        <img className="mtx-sr__picture" src="./pic.png" />
+                      </div>
+                      <div className="mtx-sr__right">
+                        <div className="mtx-sr__">
+                          <div className="mtx-section__title">{i.name}</div>
+                          <div className="mtx-sr__">{i.level}</div>
+                          <div className="mtx-sr__email">
+                            <div>{i.email}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mtx-sr__bottom">
+                      {i.skills.slice(0, 9).map((i, index) => (
+                        <div key={index} className="mtx-sr__skill">
+                          {i.name}
+                        </div>
+                      ))}
+                      ...
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
