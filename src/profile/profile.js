2515cfe4dd063bdd542dae296f58d5dd5e7c25ea
McCarthy <mamccarthy@deloitte.ie>
2019-01-21 17:27:17 +0000

    Starting to group the skills on profile

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Rating from 'react-rating';
+import _ from "lodash";
 import UserData from '../data/users.json';
 import './_profile.scss';
+import { array } from 'prop-types';
 
 class Profile extends Component {
   constructor(props) {
@@ -25,7 +27,8 @@ class Profile extends Component {
 
   render() {
     var user = this.state.userData[0];
-
+    var test = null;
+    var test1 = null;
     return (
       <div>
         <div className="mtx-logo" onClick={this.onClickHome}>
@@ -88,20 +91,28 @@ class Profile extends Component {
             skills<div>.</div>
           </div>
           <div className="mtx-block mtx-block--bottom">
-            {user.skills.map(i => (
-              <div className="mtx-skill" key={i.order}>
-                {/*<div className="mtx-section__content">{i.groupDisplayName}</div>*/}
-                <div className="mtx-skill__rating">
-                  <div className="mtx-section__header">{i.displayName}</div>
-                  <Rating
-                    initialRating={i.number}
-                    readonly={true}
-                    emptySymbol={'mtx-rating--empty'}
-                    fullSymbol={'mtx-rating--full'}
-                  />
+          
+          {           
+              test =  _(user.skills)
+              .property('group.fel')
+              .map(i => (
+                <div className="mtx-skill" key={i.name}>
+                  {<div className="mtx-section__content">{i.groupDisplayName}</div>}
+                  <div className="mtx-skill__rating">
+                    <div className="mtx-section__header">{i.displayName}</div>
+                    <Rating
+                      initialRating={i.number}
+                      readonly={true}
+                      emptySymbol={'mtx-rating--empty'}
+                      fullSymbol={'mtx-rating--full'}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+              .value()          
+            }
+            
+            
           </div>
         </div>
       </div>
